7d82439d20314574fd5dcf08bbdfac7d5e19939c
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-06-09 16:43:00 +0530

    Implemented Category App and Media Files

--- himalyancrystalgrit/settings.py
+++ himalyancrystalgrit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'himalyancrystalgrit/static')
     ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
