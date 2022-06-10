0433899c89d9f10f616c83d9ce473d0ddcb2b8b7
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-06-10 13:12:46 +0530

    Implemented Product Gallery Functionality

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, ProductGallery
 from category.models import Category
 from carts.models import CartItem
 from django.db.models import Q #or condition in filter
@@ -41,9 +41,12 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    product_gallery = ProductGallery.objects.filter(product_id = single_product.id)
+
     context = {
     'single_product' : single_product,
     'in_cart' : in_cart,
+    'product_gallery' : product_gallery,
     }
 
     return render(request, 'store/product_detail.html', context)
