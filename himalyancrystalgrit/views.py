9b3b530fffb79108b6a40e057945bb7d415b6c5e
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-06-14 04:15:29 +0530

    Banner App implemented

--- himalyancrystalgrit/views.py
+++ himalyancrystalgrit/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from store.models import Product
+from banner.models import Banner
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
+    banners = Banner.objects.all()
 
     context = {
     'products' : products,
+    'banners' : banners,
     }
 
     return render(request, 'home.html',context)
